b82cd8803531e05c58dbe3c5ead2d008771f86a5
Michal <20182549+michalbrabec@users.noreply.github.com>
2025-09-14 15:19:43 +0200

    Designed in HTML, JS and CSS to minimize dependencies

---
+++ index.js
@@ -0,0 +1,95 @@
+async function openPage(path) {
+    const { origin } = URL.parse(document.URL)
+    const response = await fetch(`${origin}/packages/page/dist/pages/${path}`)
+    const md = await response.text()
+    document.getElementById("content").innerHTML = parse(md, {});
+}
+
+async function generateSection(name, indent, sections) {
+    let buttons = ""
+    for (let section in sections) {
+        if (typeof sections[section] === 'string') {
+            buttons += `
+                <div class="menu-button button" id="side-menu-${section}" style="margin-left: ${indent}em" onClick="openPage('${sections[section]}')">
+                    <div class="side-menu-text">
+                        ${section}
+                    </div>
+                </div>
+            `;
+        }
+        else {
+            buttons += await generateSection(section, indent + 1, sections[section])
+        }
+    }
+    return `
+        <div class="side-menu-buttons" id="side-menu-buttons">
+            <div style="margin-left: ${indent-1}em">${name}</div>
+            ${buttons}
+        </div>
+    `;
+}
+
+async function openSection(sections) {
+    document.getElementById('side-menu-content').innerHTML = await generateSection("", 0, sections)
+    if(document.getElementById('side-menu').classList.contains('hide-horizontal')) {
+        toggleSideMenu()
+    }
+    await openPage(sections['Home'])
+}
+
+async function loadStructure() {
+    const { origin } = URL.parse(document.URL)
+    const response = await fetch(`${origin}/packages/page/dist/pages/dir.json`)
+    const dir = JSON.parse(await response.text())
+
+    let buttons = ""
+    for (let section in dir) {
+        buttons += `
+                <div class="menu-button button" id="top-menu-${section}">
+                    <div class="top-menu-text">
+                        ${section}
+                    </div>
+                </div>
+                `;
+    }
+    document.getElementById('top-menu-buttons').style.gridTemplateColumns=`repeat(${Object.keys(dir).length}, 1fr)`
+    document.getElementById('top-menu-buttons').innerHTML = buttons
+
+    for (let section in dir) {
+        document.getElementById(`top-menu-${section}`).addEventListener('click', async e => await openSection(dir[section]))
+    }
+
+    openSection(dir.Home)
+}
+
+function toggleSideMenu() {
+    document.getElementById('side-menu').classList.toggle('hide-horizontal')
+    document.getElementById('main').classList.toggle('expand-horizontal')
+    document.getElementById('side-menu-control-arrow').classList.toggle('flip-horizontal')
+}
+
+function toggleTopMenu() {
+    document.getElementById('side-menu').classList.toggle('extend-vertical')
+    document.getElementById('top-menu').classList.toggle('hide-vertical')
+    document.getElementById('main').classList.toggle('expand-vertical')
+    document.getElementById('top-menu-control-arrow').classList.toggle('flip-vertical')
+}
+
+async function main() {
+    //     const md = `# H1
+    // ## H2
+    // BODY
+    // `
+    //     return await parse(md, {});
+
+    await loadStructure()
+
+    {
+        document.getElementById('side-menu-control')?.addEventListener('click', toggleSideMenu)
+    }
+    {
+        document.getElementById('top-menu-control')?.addEventListener('click', toggleTopMenu)
+    }
+}
+
+main()
